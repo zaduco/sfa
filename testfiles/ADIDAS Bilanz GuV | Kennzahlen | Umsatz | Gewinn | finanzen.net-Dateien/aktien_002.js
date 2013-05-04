62ce1c7cfcd505ac6c2eb45bcdb647a0bf1945f8
zaduco <zaduco@gmail.com>
2013-05-04 08:41:43 +0200

    cleanup

--- testfiles/ADIDAS Bilanz GuV | Kennzahlen | Umsatz | Gewinn | finanzen.net-Dateien/aktien_002.js
+++
@@ -1,1 +0,0 @@
-document.write('<a target="_blank" href="http://ad.de.doubleclick.net/click;h=v8/3dc7/0/0/%2a/w;44306;0-0;0;22864698;2321-160/600;0/0/0;;~sscs=%3f"><img src="http://s0.2mdn.net/viewad/817-grey.gif" border=0 alt="Click here to find out more!"></a>');
